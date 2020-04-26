39606379fd2ce7a593c4a7a1a73fd5bf362ec1f4
Daniel Brownridge <daniel@freshnewpage.com>
2020-04-26 13:44:22 +0100

    Test pip3 installed after tests. Expect fail

---
+++ after_test.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# file: after.sh
+
+testFoundCommandPip3() {
+    cmd="fish"
+    output=$(command -v ${cmd})
+    rtrn=$?
+    assertTrue "${cmd} missing: ${output}" ${rtrn}
+}
+
+# shellcheck disable=SC1091
+. ./shunit2
+
