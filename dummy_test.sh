bd7321544919e6edd09491ab8f1ee6b13d724487
Daniel Brownridge <daniel@freshnewpage.com>
2022-10-05 08:58:46 +0100

    Update for 20.04 Focal Fossa
    Remove tests
    Simplify install method
    Remove ansible-lint

--- dummy_test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-# file: tests/dummy.sh
-
-testEquality() {
-    assertEquals 1 1
-}
-
-# shellcheck disable=SC1091
-. ./shunit2
--- install-test-dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# file: install-test-dependencies.sh
-wget https://raw.githubusercontent.com/kward/shunit2/v2.1.8/shunit2
